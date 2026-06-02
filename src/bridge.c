bc0d79603e2a098ec562be7e3f5b31eea33b71d9
Ramsey McGrath <ramseymcgrath@gmail.com>
2026-06-02 02:07:22 -0400

    fix: include <io.h> on Windows for _isatty/_fileno declarations

--- src/bridge.c
+++ src/bridge.c
@@ -28,6 +28,7 @@
 #ifdef _WIN32
 #  define WIN32_LEAN_AND_MEAN
 #  include <windows.h>
+#  include <io.h>          /* _isatty, _fileno */
 #  define HOME_ENV "USERPROFILE"
 #else
 #  include <unistd.h>
